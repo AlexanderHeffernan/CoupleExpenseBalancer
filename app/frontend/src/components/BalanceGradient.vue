5ee5e6945f3774e71fad206d68224825aed05e64
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-04-22 11:40:59 +1200

    Created visualisation of balance

---
+++ app/frontend/src/components/BalanceGradient.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['u1deficit', 'u2deficit']);
+
+function getBalancePoint() {
+  if (props.u1deficit === 0 && props.u2deficit === 0) {
+    return 50;
+  }
+  return props.u1deficit / (props.u1deficit + props.u2deficit) * 100;
+
+}
+
+</script>
+
+<template>
+    <div style="border: 16px solid #FFCCCC;" class="w-64 h-64 rounded-full" :style="{ background: `linear-gradient(to right, #FF6B6B ${getBalancePoint()}%, #A9A9A9 0%)` }">
+        <div style="width: 150%; translate: -17% 140%;" class="flex justify-center items-center shadow-2xl bg-accent h-1/4 rounded-3xl">
+            <p class="text-primary text-2xl font-bold w-1/2 text-center">${{ u1deficit }}</p>
+            <p class="text-text text-2xl font-bold w-1/2 text-center">${{ u2deficit }}</p>
+        </div>
+    </div>
+</template>
--- app/frontend/src/pages/HomePage.vue
+++ app/frontend/src/pages/HomePage.vue
@@ -1,29 +1,15 @@
 <script setup>
 import { defineProps } from 'vue';
 import PageHeader from '../components/PageHeader.vue';
+import BalanceGradient from '../components/BalanceGradient.vue';
 
-const props = defineProps(['u1deficit', 'u2deficit']);
-
-/**
- * Calculate the angle of the conic gradient based on the user deficits
- * @param {number} user_id
- */
-function getAngle(user_id) {
-  const totalDeficit = props.u1deficit + props.u2deficit;
-  // If both expenses are zero, return 180 degrees (perfect split)
-  return totalDeficit === 0 ? 180 : user_id === 1
-      ? (props.u1deficit / totalDeficit) * 360
-      : (props.u2deficit / totalDeficit) * 360;
-}
+defineProps(['u1deficit', 'u2deficit']);
 
 </script>
 
 <template>
     <div class="w-full h-full flex flex-col items-center">
       <PageHeader pageHeading="Home" />
-      <div class="w-64 h-64 rounded-full flex justify-center items-center" :style="{ backgroundImage: `conic-gradient(red ${getAngle(1)}deg, blue 0deg)` }">
-        <p class="w-1/2 text-center">${{ u2deficit }}</p>
-        <p class="w-1/2 text-center">${{ u1deficit }}</p>
-      </div>
+      <BalanceGradient :u1deficit="u1deficit" :u2deficit="u2deficit" />
     </div>
 </template>
