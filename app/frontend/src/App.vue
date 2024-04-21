56b498f169a648a364a7584ac57bf04313b7cc59
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-04-21 19:12:17 +1200

    Add epense form, which emits data back to App.vue

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue' //onMounted, watch
+import { ref, onMounted, watch } from 'vue'
 import HomePage from './pages/HomePage.vue';
 import AddPage from './pages/AddPage.vue';
 import BalancePage from './pages/BalancePage.vue';
@@ -18,11 +18,32 @@ function getUserExpenses(user) {
     }, 0);
 }
 
+function getBalance() {
+    const u1expenses = getUserExpenses(1), u2expenses = getUserExpenses(2);
+    const difference = Math.abs(u1expenses - u2expenses);
+    if (u1expenses > u2expenses) {
+        return { user: 1, amount: difference };
+    } else if (u2expenses > u1expenses) {
+        return { user: 2, amount: difference };
+    } else {
+        return { user: 0, amount: 0 };
+    }
+}
+
+function handleAddExpense(expenseData) {
+    expenses.value.push({ id: expenses.value.length + 1, description: expenseData.description, user: expenseData.user, amount: expenseData.amount, balance: false});
+    localStorage.setItem('expenses', JSON.stringify(expenses.value));
+}
+
 
 onMounted(() => {
     expenses.value = JSON.parse(localStorage.getItem('expenses')) || [];
-    expenses.value.push({ id: expenses.value.length + 1, description: 'Test', user: 1, amount: 100, balance: false });
 });
+
+watch(expenses, newVal => {
+    localStorage.setItem('expenses', JSON.stringify(newVal));
+}, { deep: true });
+
 </script>
 
 <template>
@@ -31,8 +52,8 @@ onMounted(() => {
         <!-- Current page view-->
         <div class="w-full h-[calc(100%-64px)] bg-green-500">
             <HomePage v-if="currentPage == 'home'" :u1expense="getUserExpenses(1)" :u2expense="getUserExpenses(2)" />
-            <AddPage v-if="currentPage == 'add'" />
-            <BalancePage v-if="currentPage == 'balance'" />
+            <AddPage v-if="currentPage == 'add'" @addExpense="handleAddExpense" />
+            <BalancePage v-if="currentPage == 'balance'" @balanceData="getBalance()" />
         </div>
         <!-- Navbar -->
         <div class="fixed bottom-0 left-0 w-full h-16 bg-white">
--- app/frontend/src/pages/AddPage.vue
+++ app/frontend/src/pages/AddPage.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref, defineEmits } from 'vue';
+const emit = defineEmits(['addExpense'])
+
+const description = ref('');
+const user = ref();
+const amount = ref();
+
+const addExpense = () => {
+  const expenseData = {
+    description: description.value,
+    user: user.value,
+    amount: parseFloat(amount.value)
+  };
+
+  emit('addExpense', expenseData);
+};
 
 </script>
 
 <template>
     <div class="w-full h-full">
       <p>Add page</p>
+      <input type="text" placeholder="Description" v-model="description" />
+      <input type="number" placeholder="User" v-model="user" />
+      <input type="number" placeholder="Amount" v-model="amount" />
+      <button @click="addExpense">Add</button>
     </div>
 </template>
 
