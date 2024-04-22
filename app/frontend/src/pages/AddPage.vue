e8a3c6eb2903693dfd3d6ffe80a08a76585d6727
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-04-22 12:55:40 +1200

    Initial setup of styling for balance page

--- app/frontend/src/pages/AddPage.vue
+++ app/frontend/src/pages/AddPage.vue
@@ -29,25 +29,23 @@ const addTransaction = () => {
 <template>
     <div class="w-full h-full flex flex-col items-center">
       <PageHeader pageHeading="Add Expense" />
-      <div class="shadow-xl p-5 rounded-2xl w-[80%] bg-widget h-[300px]">
-        <div class="flex flex-col items-center">
-          <!-- Description input -->
-          <label class="text-left w-full font-bold mb-2">Description</label>
-          <input class="w-full rounded-lg p-2 mb-2" type="text" placeholder="Lunch at McDonalds" v-model="description" />
-          <div class="flex gap-5 mb-10">
-            <div>
-              <!-- User ID input -->
-              <label class="text-left w-full font-bold">User_ID</label>
-              <input class="w-full rounded-lg mt-2 p-2" type="number" placeholder="1" v-model="user_id" />
-            </div>
-            <div>
-              <!-- Amount input -->
-              <label class="text-left w-full font-bold">Amount</label>
-              <input class="w-full rounded-lg mt-2 p-2" type="number" placeholder="100" v-model="amount" />
-            </div>
+      <div class="flex flex-col items-center shadow-xl p-5 rounded-2xl w-[80%] bg-widget h-[300px]">
+        <!-- Description input -->
+        <label class="text-left w-full font-bold mb-2">Description</label>
+        <input class="w-full rounded-lg p-2 mb-2" type="text" placeholder="Lunch at McDonalds" v-model="description" />
+        <div class="flex gap-5 mb-10">
+          <div>
+            <!-- User ID input -->
+            <label class="text-left w-full font-bold">User_ID</label>
+            <input class="w-full rounded-lg mt-2 p-2" type="number" placeholder="1" v-model="user_id" />
           </div>
-          <button class="text-lg bg-primary pl-10 pr-10 pt-2 pb-2 rounded-xl text-white font-semibold" @click="addTransaction">Add</button>
+          <div>
+            <!-- Amount input -->
+            <label class="text-left w-full font-bold">Amount</label>
+            <input class="w-full rounded-lg mt-2 p-2" type="number" placeholder="100" v-model="amount" />
+          </div>
         </div>
+        <button class="text-lg bg-primary pl-10 pr-10 pt-2 pb-2 rounded-xl text-white font-semibold" @click="addTransaction">Add</button>  
       </div>
     </div>
 </template>
--- app/frontend/src/pages/BalancePage.vue
+++ app/frontend/src/pages/BalancePage.vue
@@ -8,9 +8,11 @@ defineEmits(['balanceConfirmed'])
 </script>
 
 <template>
-    <div class="w-full h-full">
+    <div class="w-full h-full flex flex-col items-center">
         <PageHeader pageHeading="Balance" />
-        <p>User {{ balanceData.user_id }} owes: {{ balanceData.amount }}</p>
-        <button @click="$emit('balanceConfirmed')">Confirm</button>
+        <div class="flex flex-col items-center shadow-xl p-5 rounded-2xl w-[80%] bg-widget h-[300px]">
+            <p>User {{ balanceData.user_id }} owes: ${{ balanceData.amount }}</p>
+            <button class="text-lg bg-primary pl-10 pr-10 pt-2 pb-2 rounded-xl text-white font-semibold" @click="$emit('balanceConfirmed')">Confirm</button>
+        </div>
     </div>
 </template>
