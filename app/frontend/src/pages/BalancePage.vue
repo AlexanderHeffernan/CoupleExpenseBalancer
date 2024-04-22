d7a0e9a3969f26970602f77fc592369e0b312985
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-04-22 15:15:56 +1200

    Updated styling of balance page for when it is balanced

--- app/frontend/src/pages/BalancePage.vue
+++ app/frontend/src/pages/BalancePage.vue
@@ -22,6 +22,11 @@ function convertAmount(amount) {
 }
 
 function changeBackground(exiting) {
+    if (isBalanced()) {
+        document.querySelector('.app').style.backgroundColor = 'white';
+        document.querySelector('.balance-nav-button').style.border = 'none';
+        return;
+    }
     if (!exiting) {
         if ((stage.value === 0 && props.balanceData.user_id === 1)
             || (stage.value === 1 && props.balanceData.user_id === 2)) {
@@ -57,11 +62,15 @@ function getWidgetColor() {
     }
 }
 
+function isBalanced() {
+    return props.balanceData.u1deficit === props.balanceData.u2deficit;
+}
+
 </script>
 
 <template>
     <PageHeader pageHeading="Balance" />
-    <div class="w-full h-[calc(100%-90px)] flex flex-col justify-center items-center">
+    <div v-if="!isBalanced()" class="w-full h-[calc(100%-90px)] flex flex-col justify-center items-center">
         <div :class="getWidgetColor() + ` flex flex-col items-center shadow-xl p-5 rounded-2xl w-[80%]`">
             <p class="font-bold text-xl mb-5">Summary</p>
             <div class="bg-white w-full rounded-xl p-5 mb-5">
@@ -98,5 +107,14 @@ function getWidgetColor() {
         </div>
         <button v-if="stage === 0" :class="getWidgetColor() + ` text-lg pl-10 pr-10 pt-2 pb-2 rounded-xl text-text font-semibold mt-5`" @click="updateStage">Payment Sent</button>
         <button v-else :class="getWidgetColor() + ` text-lg pl-10 pr-10 pt-2 pb-2 rounded-xl text-text font-semibold mt-5`" @click="updateStage">Payment Received</button>
+    </div>
+    <div v-else class="w-full h-[calc(100%-90px)] flex flex-col justify-center items-center">
+        <div class="bg-accent flex flex-col items-center shadow-xl p-5 rounded-2xl w-[80%]">
+            <div class="w-1/2 rounded-full bg-widget">
+                <svg class="w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="fill-primary" d="M256 64c53 0 96 43 96 96s-43 96-96 96s-96 43-96 96s43 96 96 96C150 448 64 362 64 256S150 64 256 64zm0 448A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm32-352a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+            </div>
+            <br />
+            <p class="font-bold text-2xl text-text text-center">Expenses are<br/> Balanced</p>
+        </div>
     </div>
 </template>
