2467e7a78a03c043cbe9f32f7a07f6bafb31509b
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-05-01 23:27:56 +1200

    Web app not compatable to be a PWA. Web app is full screen when saved to home page on ios safari

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/tailwind.css'
 import './main.css'
+import './registerServiceWorker'
 
 createApp(App).mount('#app')
