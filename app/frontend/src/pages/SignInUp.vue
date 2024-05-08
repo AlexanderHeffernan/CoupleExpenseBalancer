8f528ee34e13b16bd46ce56e7c3c476466a3e454
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-05-08 15:10:31 +1200

    Tidying up SignInUp code

--- app/frontend/src/pages/SignInUp.vue
+++ app/frontend/src/pages/SignInUp.vue
@@ -1,37 +1,30 @@
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
 import { login, signup } from '../utils/userAccount.js';
 
-const emit = defineEmits(['loggedIn'])
-
-const signUpName = ref('');
-const signUpEmail = ref('');
-const signUpPassword = ref('');
-
-const signInEmail = ref('');
-const signInPassword = ref('');
+const signUpName = ref(''), signUpEmail = ref(''), signUpPassword = ref('');
+const signInEmail = ref(''), signInPassword = ref('');
 
 function handleSignup() {
     signup(signUpName.value, signUpEmail.value, signUpPassword.value);
 }
 
 function handleLogin() {
-    if (login(signInEmail.value, signInPassword.value))
-        emit('loggedIn');
+    login(signInEmail.value, signInPassword.value)
 }
 </script>
 
 <template>
     <!-- Sign in/up form -->
     <div class="flex flex-row justify-center items-center h-[100vh]">
-        <form  class="flex flex-col items-start h-28" @submit.prevent="handleSignup">
+        <form class="flex flex-col items-start h-28" @submit.prevent="handleSignup">
             <h2 class="text-red-500">Sign Up</h2>
             <input type="text" v-model="signUpName" placeholder="Name" required>
             <input type="email" v-model="signUpEmail" placeholder="Email" required>
             <input type="password" v-model="signUpPassword" placeholder="Password" required>
             <button type="submit">Sign up</button>
         </form>
-        <form class="flex flex-col items-start h-28"  @submit.prevent="handleLogin">
+        <form class="flex flex-col items-start h-28"  @submit.prevent="handleLogin()">
             <h2>Sign In</h2>
             <input type="email" v-model="signInEmail" placeholder="Email" required>
             <input type="password" v-model="signInPassword" placeholder="Password" required>
